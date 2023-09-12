7ad92730f23b3a81e280447ee5118e5cfa4f1db3
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-09-12 17:14:35 +0400

    Half of the third page is done

---
+++ main/main.js
@@ -0,0 +1,23 @@
+//  // Get the current URL
+//  const currentURL = window.location.href;
+
+//  // Define the URL of the target page
+//  const targetURL = 'http://127.0.0.1:5500/main/main.html'; // Replace with your target page URL
+
+//  // Get the link element by its ID
+//  const linkElement = document.getElementById('link-home');
+//  const imageHome = document.getElementById('img-home')
+
+// //  // Check if the current URL matches the target URL
+// //  if (currentURL === targetURL) {
+// //    // Change the style of the link on the target page
+// //    imageHome.style.backgroundColor  = 'red'; // Change link color to red as an example
+// //    imageHome.style.color  = 'white'; // Change link color to red as an example
+// //    // You can apply other style changes as needed
+// //  }
+
+// const svgElement = document.getElementById("mySVG");
+
+// svgElement.addEventListener("", function () {
+//     svgElement.classList.toggle("clicked");
+// });
