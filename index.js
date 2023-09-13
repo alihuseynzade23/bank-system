5776235e7d73104deccf147f8e802fe4b4240a02
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-09-13 17:19:55 +0400

    End of the project

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-const error = document.getElementById('logError')
+const error = document.getElementById("logError");
 
 function checkTheData() {
-    const savedEmail = localStorage.getItem('email');
-    const savedPassword = localStorage.getItem('password');
+    const savedEmail = localStorage.getItem("email");
+    const savedPassword = localStorage.getItem("password");
 
-    const emailInputValue = document.getElementById('logEmail').value;
-    const passInputValue = document.getElementById('logPass').value;
+    const emailInputValue = document.getElementById("logEmail").value;
+    const passInputValue = document.getElementById("logPass").value;
 
-
-    // Check if the entered email and password match the saved values
     if (emailInputValue === savedEmail && passInputValue === savedPassword) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
 
-document.getElementById('signInSubmit').addEventListener('click', () => {
+document.getElementById("signInSubmit").addEventListener("click", () => {
     if (checkTheData()) {
-        error.textContent = '';
-        window.location.href = 'http://127.0.0.1:5500/main/main.html';
+        error.textContent = "";
+        window.location.href = "http://127.0.0.1:5500/main/main.html";
     } else {
-        error.textContent = 'Şifrə və ya login düzgün yazılmayıb'
+        error.textContent = "Şifrə və ya login düzgün yazılmayıb";
     }
-    
 });
-
-// Load saved values from local storage when the page loads
-// emailInput.value = localStorage.getItem('email') || '';
-// passInput.value = localStorage.getItem('password') || '';
--- registration/registration.js
+++ registration/registration.js
@@ -1,32 +1,24 @@
-// Get references to the input elements
-const emailInput = document.getElementById('email');
-const passInput = document.getElementById('pass');
+const emailInput = document.getElementById("email");
+const passInput = document.getElementById("pass");
 
-
-// Function to save the input values to local storage
 function saveToLocalStorage() {
-    localStorage.setItem('email', emailInput.value);
-    localStorage.setItem('password', passInput.value);
+    localStorage.setItem("email", emailInput.value);
+    localStorage.setItem("password", passInput.value);
 }
 
-
-emailInput.addEventListener('input', saveToLocalStorage);
-passInput.addEventListener('input', saveToLocalStorage);
-
+emailInput.addEventListener("input", saveToLocalStorage);
+passInput.addEventListener("input", saveToLocalStorage);
 
 function validateForm() {
-    // Call all validation functions
     validateName("fname", "nameError");
     validateName("sname", "surnameError");
     validateEmail();
-    validatePassword('pass', 'passError');
-    passwordSimilarity('pass', 'pass2', 'passError2');
+    validatePassword("pass", "passError");
+    passwordSimilarity("pass", "pass2", "passError2");
 
-    // Check if there are any error messages
-    const errorElements = document.querySelectorAll('.error');
+    const errorElements = document.querySelectorAll(".error");
     for (const errorElement of errorElements) {
-        if (errorElement.textContent !== '') {
-            // If any error messages are present, prevent form submission
+        if (errorElement.textContent !== "") {
             return false;
         }
     }
@@ -35,103 +27,92 @@ function validateForm() {
     return true;
 }
 
-document.getElementById('registrationSubmit').addEventListener('click', () => {
+document.getElementById("registrationSubmit").addEventListener("click", () => {
     if (validateForm()) {
-        // If the form is valid, perform the redirection
-        window.location.href = 'http://127.0.0.1:5500/index.html';
+        window.location.href = "http://127.0.0.1:5500/index.html";
     }
 });
 
-// Name and surname validator
 function validateName(inputId, errorId) {
-    const input = document.getElementById(inputId)
-    const error = document.getElementById(errorId)
+    const input = document.getElementById(inputId);
+    const error = document.getElementById(errorId);
     const nameRegex = /^[A-Za-z]+$/;
 
-    if(input.value === '') {
-        error.textContent = 'Xananı doldurun'
-    } else if(!nameRegex.test(input.value)) {
-        error.textContent = 'Səhv input. Ancaq hərflər qəbul olunur.'
+    if (input.value === "") {
+        error.textContent = "Xananı doldurun";
+    } else if (!nameRegex.test(input.value)) {
+        error.textContent = "Səhv input. Ancaq hərflər qəbul olunur.";
     } else {
-        error.textContent = ''
+        error.textContent = "";
     }
 }
 
 document.getElementById("fname").addEventListener("input", () => {
     validateName("fname", "nameError");
-  });
-  
-  document.getElementById("sname").addEventListener("input", () => {
+});
+
+document.getElementById("sname").addEventListener("input", () => {
     validateName("sname", "surnameError");
-  });
+});
 
-
-// Email validator
 function validateEmail() {
-    const input = document.getElementById("email")
-    const error = document.getElementById("emailError")
+    const input = document.getElementById("email");
+    const error = document.getElementById("emailError");
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(input.value === '') {
-        error.textContent = 'Xananı doldurun'
-    } else if(!emailRegex.test(input.value)) {
-        error.textContent = 'Zəhmət düzgün email formatı yazardiz'
+    if (input.value === "") {
+        error.textContent = "Xananı doldurun";
+    } else if (!emailRegex.test(input.value)) {
+        error.textContent = "Zəhmət düzgün email formatı yazardiz";
     } else {
-        error.textContent = ''
+        error.textContent = "";
     }
 }
 
-document.getElementById('email').addEventListener('input', validateEmail)
+document.getElementById("email").addEventListener("input", validateEmail);
 
-
-// Password validator
 function validatePassword(inputId, errorId) {
-    const input = document.getElementById(inputId)
-    const error = document.getElementById(errorId)
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
+    const input = document.getElementById(inputId);
+    const error = document.getElementById(errorId);
+    const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
 
-    if(input.value === '') {
-        error.textContent = 'Xananı doldurun'
-    } else if(!passwordRegex.test(input.value)) {
-        error.textContent = 'Şifrə yalnız latın hərfləri olmalıdır və rəqəmlər'
+    if (input.value === "") {
+        error.textContent = "Xananı doldurun";
+    } else if (!passwordRegex.test(input.value)) {
+        error.textContent = "Şifrə yalnız latın hərfləri olmalıdır və rəqəmlər";
     } else {
-        error.textContent = ''
+        error.textContent = "";
     }
-
 }
 
-document.getElementById('pass').addEventListener('input', () => {
-    validatePassword('pass', 'passError')
-  })
+document.getElementById("pass").addEventListener("input", () => {
+    validatePassword("pass", "passError");
+});
 
-
-
-//Passwords similarity validator
 function passwordSimilarity(firstPassId, secondPassId, errorId) {
     const pass1 = document.getElementById(firstPassId).value;
     const pass2 = document.getElementById(secondPassId).value;
     const error = document.getElementById(errorId);
-    
-    error.textContent = ''; 
+
+    error.textContent = "";
 
     if (pass1.length !== pass2.length) {
-        error.textContent = 'Şifrə eyni deyil';
+        error.textContent = "Şifrə eyni deyil";
     } else {
         for (let i = 0; i < pass1.length; i++) {
             if (pass1[i] !== pass2[i]) {
-                error.textContent = 'Şifrə eyni deyil';
-                break; 
+                error.textContent = "Şifrə eyni deyil";
+                break;
             }
         }
     }
 }
 
-document.getElementById('pass2').addEventListener('input', () => {
-    passwordSimilarity('pass', 'pass2', 'passError2')
-})
+document.getElementById("pass2").addEventListener("input", () => {
+    passwordSimilarity("pass", "pass2", "passError2");
+});
 
-
-
-document.getElementById('registrationSubmit').addEventListener('click', () => {
-    console.log('hello')
-})
+document.getElementById("registrationSubmit").addEventListener("click", () => {
+    console.log("hello");
+});
